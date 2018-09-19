c637af6dc2169244b06e65d6cee29b41d03ac3ec
Easton Liu <yun-sheng.liu@hpe.com>
2018-09-19 13:13:44 +0800

    Add dist.

---
+++ dist/io/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var file_system_1 = require("./file_system");
+exports.fileSystem = file_system_1.fileSystem;
+//# sourceMappingURL=index.js.map
